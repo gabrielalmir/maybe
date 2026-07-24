412dbb8d06a8a362401d057c6cdff389692658d6
gabrielalmir <gabr.almir@gmail.com>
2026-07-24 20:08:34 -0300

    fix: declare worker cli arguments

--- src/Async/worker.php
+++ src/Async/worker.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 require __DIR__ . DIRECTORY_SEPARATOR . 'WorkerRuntime.php';
 require __DIR__ . DIRECTORY_SEPARATOR . 'Ipc.php';
 
+/** @var array<int,string> $argv */
 \Maybe\Async\WorkerRuntime::run(
     $argv[1],
     $argv[2],
